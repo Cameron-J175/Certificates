cac609c483f915ab2550b0d8208f8edba31d6de9
Cameron-J175 <cameron.jennings24@student.cart.org>
2025-02-07 13:04:35 -0800

    Finished

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function dynamics() {
  let selectedValue = change.options[change.selectedIndex].value;
     if (selectedValue == "People's Person") {
         document.getElementById("Description").innerHTML = "ENJOY BEING THE MOST POPULAR MAN OR WOMAN IN ANY ROOM. WITH CHARM THAT CAPTIVATES AND CONFIDENCE THAT DEMANDS RESPECT. ENJOY YOUR NEW LIFE.";
-        document.getElementById("CEOName").innerHTML = "JOHN CHARLES"
+        document.getElementById("CEOName").innerHTML = "- JOHN CHARLES"
         document.getElementById("CEOTitle").innerHTML = "CEO OF POPULAR GUYS" 
    } else if (selectedValue == "Person") {
         document.getElementById("Description").innerHTML = "ENJOY BEING A MAN OR WOMAN IN ANY ROOM. WITH CHARM AND SOME CONFIDENCE. ENJOY."
-        document.getElementById("CEOName").innerHTML = "JONAH CHARLES"
+        document.getElementById("CEOName").innerHTML = "- JONAH CHARLES"
         document.getElementById("CEOTitle").innerHTML = "CEO OF GUYS" 
    } else if (selectedValue == "Being") {
         document.getElementById("Description").innerHTML = "ENJOY EXISTING IN ANY ROOM. WITH SOMETHING OR SOMETHING ELSE. GOODLUCK."
-        document.getElementById("CEOName").innerHTML = "DEREK"
+        document.getElementById("CEOName").innerHTML = "- DEREK"
         document.getElementById("CEOTitle").innerHTML = "CEO OF EXISTING"
    } else if (selectedValue == "Choose Title"){
     alert("Please select a certificate");
